29f405aa8d884812368e1d634171d9838f981f80
manuel ivagnes <ivagnes.manuel@gmail.com>
2018-12-02 16:28:29 +0100

    add join features + comments feature + user events page

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # QuickEvent
-nodejs with express application for computer networks course at university of Sapienza 
+nodejs with express application for computer networks course at university of Sapienza
+
+QuickEvent is an application to easy share and join events
+
+Implementation options:
+- The event creator is not automatically added in their event, he/she can also decide if join the event or not
+- It's possible to create more events with same property
--- routes/events.js
+++ routes/events.js
@@ -7,43 +7,70 @@ const {ensureAuthenticated} = require('../helpers/auth');
 
 // My events route
 router.get('/myEvents', ensureAuthenticated, (req,res) => {
-  Event.find({creator: req.user.id})
+  //find events created
+  Event.find({
+    creator: req.user.id
+    })
     .sort({dateCreation:'desc'})
     .then(myevents => {
-      res.render('events/myEvents', {
-        myevents:myevents
-      });
+
+      //find events joined
+      User.findOne({
+         _id:req.user.id
+      })
+      .populate('events')
+      .then(userfound => {
+
+        //render both
+        res.render('events/myEvents', {
+          myevents:myevents,
+          eventsjoined:userfound.events
+        });
+      })
     });
-})
+});
 
 // Create event route
 router.get('/createEvent', ensureAuthenticated, (req,res) => {
   res.render('events/createEvent');
 })
 
-// Show Single evnet
+// Show Single event
 router.get('/show/:id', (req, res) => {
   Event.findOne({
     _id: req.params.id
-  })
-  .populate('creator')     //to access creator info
-  .then(event => {
-    res.render('events/show', {
-      event:event
+    })
+    .populate('creator')     //to access creator info
+    .populate('comments.commentUser')
+    .then(event => {
+      res.render('events/show', {
+        event:event
+      });
     });
-  });
+});
+
+// List stories from a user
+router.get('/user/:userId', (req, res) => {
+  Event.find({
+    creator: req.params.userId
+    })
+    .then(events => {
+      res.render('events/userEvents', {
+        events:events
+      });
+    });
 });
 
 // Edit Event form
 router.get('/editEvent/:id', ensureAuthenticated, (req,res) => {
   Event.findOne({
     _id: req.params.id
-  })
-  .then(event => {
-    res.render('events/editEvent', {
-      event:event
+    })
+    .then(event => {
+      res.render('events/editEvent', {
+        event:event
+      });
     });
-  });
 })
 
 
@@ -85,8 +112,6 @@ router.post('/createEvent', (req, res) => {
       creator: req.user.id
     }
 
-    console.log(newEvent.date);
-
     new Event(newEvent)
       .save()
       .then(event => {
@@ -101,60 +126,141 @@ router.post('/createEvent', (req, res) => {
 router.put('/:id', (req, res) => {
   Event.findOne({
     _id: req.params.id
-  })
-  .then(event => {
-    let errors = [];;
+    })
+    .then(event => {
+      let errors = [];;
 
-    //server side validation
-    if(!req.body.name){
-      errors.push({text:'Please add the name'});
-    }
-    if(!req.body.date){
-      errors.push({text:'Please add the date'});
-    }
-    if(!req.body.time){
-      errors.push({text:'Please add the time'});
-    }
-    if(!req.body.location){
-      errors.push({text:'Please add the location'});
-    }
+      //server side validation
+      if(!req.body.name){
+        errors.push({text:'Please add the name'});
+      }
+      if(!req.body.date){
+        errors.push({text:'Please add the date'});
+      }
+      if(!req.body.time){
+        errors.push({text:'Please add the time'});
+      }
+      if(!req.body.location){
+        errors.push({text:'Please add the location'});
+      }
 
-    if(errors.length > 0){
-      res.render('events/createEvent', {
-        errors: errors,
-        name: req.body.name,
-        date: req.body.date,
-        time: req.body.time,
-        location: req.body.location,
-        details: req.body.details,
-      });
-    } else {
+      if(errors.length > 0){
+        res.render('events/createEvent', {
+          errors: errors,
+          name: req.body.name,
+          date: req.body.date,
+          time: req.body.time,
+          location: req.body.location,
+          details: req.body.details,
+        });
+      } else {
 
-      //update values
-      event.name = req.body.name,
-      event.date = req.body.date + ' ' + req.body.time,
-      event.location = req.body.location,
-      event.details = req.body.details,
-      event.creator = req.user.id
+        //update values
+        event.name = req.body.name,
+        event.date = req.body.date + ' ' + req.body.time,
+        event.location = req.body.location,
+        event.details = req.body.details,
+        event.creator = req.user.id
+
+        event.save()
+        .then(event => {
+          req.flash('success_msg', 'Event updated');
+          res.redirect('/events/myEvents');
+        })
+      }
+    });
+});
+
+
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Event.findOne({
+    _id: req.params.id
+    })
+    .then(event => {
+      const newComment = {
+        commentBody: req.body.commentBody,
+        commentUser: req.user.id
+      }
+
+      // Add to comments array (use unshift instead of push to put at the top)
+      event.comments.unshift(newComment);
 
       event.save()
-      .then(event => {
-        req.flash('success_msg', 'Event updated');
-        res.redirect('/events/myEvents');
+        .then(event => {
+          res.redirect(`/events/show/${event.id}`);
+        });
+    });
+});
+
+
+//join event Process
+router.put('/join/:id', (req, res) => {
+  //to add user in event (joiners) list
+  Event.findOne({
+    _id: req.params.id
+    })
+    .then(event => {
+      event.joiners.push(req.user.id);
+
+      //to add also in the user (events) list
+      User.findOne({
+        _id: req.user.id
       })
-    }
-  });
+      .then(user => {
+        user.events.unshift(event);
+        user.save()
+      });
+
+      event.save()
+        .then(event => {
+          req.flash('success_msg', 'Event joined');
+          res.redirect('/events/myEvents');
+        });
+
+    });
+});
+
+
+//leave event Process
+router.put('/delete/:id', (req, res) => {
+  //to delete user in event (joiners) list
+  Event.findOne({
+    _id: req.params.id
+    })
+    .then(event => {
+      console.log('aaa');
+      event.joiners.pull(req.user.id);
+
+      //to delete also in the user (events) list
+      User.findOne({
+          _id: req.user.id
+        })
+        .then(user => {
+          user.events.pull(event);
+          user.save()
+        });
+
+      event.save()
+        .then(event => {
+          req.flash('error_msg', 'Event left');
+          res.redirect('/events/myEvents');
+        });
+
+    });
 });
 
 
 //delete event
 router.delete('/:id', (req, res) => {
-  Event.remove({
+  Event.deleteOne({
     _id: req.params.id
-  })
-   .then(() => {
-      req.flash('success_msg', 'Event removed');
+    })
+    .then(() => {
+      req.flash('error_msg', 'Event removed');
       res.redirect('/events/myEvents');
     });
 });
+
+
 module.exports = router;
